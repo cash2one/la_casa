06d103f42955593fdc856db64d14735532877be3
Aditya Kalyanakrishnan <aditya.kalyanakrishnan@knowlarity.com>
2016-03-29 22:25:59 +0530

    updated models

--- website/models.py
+++ website/models.py
@@ -5,10 +5,7 @@ from django.db import models
 # Create your models here.
 class Property(models.Model):
     name = models.CharField(max_length=30)
-    address = models.TextField()
     location = models.CharField(max_length=100)
-    price_rouble = models.IntegerField()
-    price_usd = models.IntegerField()
     contact = models.CharField(max_length=15)
     email_id = models.EmailField()
 
