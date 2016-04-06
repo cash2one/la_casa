1e1488247460ee23ff031489f362ef3e678f5c22
Adi Krishnan <aditya.k2190@gmail.com>
2016-04-06 09:20:27 +0530

    extended templates from base app

--- the_cottage/settings.py
+++ the_cottage/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'website',
     'naracottage',
+    'website',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
