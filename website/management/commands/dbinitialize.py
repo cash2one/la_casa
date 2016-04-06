1e20fe729075d5763e8d657d9f05cd36f26fd1e2
Adi Krishnan <aditya.k2190@gmail.com>
2016-04-06 09:13:44 +0530

    created initialization command for populating db

---
+++ website/management/commands/dbinitialize.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from website.models import Property, Photos, Sitetext
+
+
+class Command(BaseCommand):
+    help = 'Initializes database with default values for all pages'
+
+    def handle(self, *args, **options):
+        try:
+            name = 'narofominsk'
+            location = '55.406073, 36.685741'
+            contact = '1234567890'
+            email_id = 'random@random.com'
+            prop = Property.objects.create(name=name, location=location,
+                                           contact=contact, email_id=email_id)
+            prop.save()
+            self.stdout.write(self.style.SUCCESS('%s property added.' % name))
+        except Exception as ex:
+            self.stdout.write(self.style.ERROR('Could not add %s property.' % name))
