ca293a557b1ebceda8bdbd22ea4f22f7d58f1263
Adi Krishnan <aditya.k2190@gmail.com>
2016-06-22 04:50:52 +0000

    added basic page to arbatflat

--- arbatflat/views.py
+++ arbatflat/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from website.models import Property
 
-# Create your views here.
+
+def home(request):
+    prop = Property.objects.get(name='narofominsk')
+    template = loader.get_template('home.html')
+    location = prop.location
+    latitude, longitude = location.split(',')
+    context = {
+                'name': prop.name,
+                'latitude': latitude,
+                'longitude': longitude,
+                'phone': prop.contact,
+              }
+    return HttpResponse(template.render(context, request))
