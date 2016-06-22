20e3209837d2458ffa5b6ea818da22fd5f2ab4d5
Adi Krishnan <aditya.k2190@gmail.com>
2016-06-22 03:34:16 +0000

    virtual host setup test

---
+++ naracottage/settings.py
@@ -0,0 +1,8 @@
+from settings import *
+
+ALLOWED_HOSTS = ['test.naracottage.com', 'www.test.naracottage.com']
+
+ROOT_URLCONF = 'naracottage.urls'
+
+WSGI_APPLICATION = 'naracottage.wsgi.application'
+
