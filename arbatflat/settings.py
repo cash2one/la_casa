84fac682b37a63af31ecbc6174e8b3a0c2fc352e
Adi Krishnan <aditya.k2190@gmail.com>
2016-06-22 04:21:43 +0000

    added arbatflatg

---
+++ arbatflat/settings.py
@@ -0,0 +1,8 @@
+from the_cottage.settings import *
+
+ALLOWED_HOSTS = ['test.arbatflat.com', 'www.test.arbatflat.com']
+
+ROOT_URLCONF = 'arbatflat.urls'
+
+WSGI_APPLICATION = 'arbatflat.wsgi.application'
+
--- the_cottage/settings.py
+++ the_cottage/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'naracottage',
+    'arbatflat',
     'website',
     'django.contrib.admin',
     'django.contrib.auth',
