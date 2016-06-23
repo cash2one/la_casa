05832bf5515dfe2cfa7fcdcb5ed3f62a2aeb7a55
Adi Krishnan <aditya.k2190@gmail.com>
2016-06-23 05:00:57 +0000

    updated naracottage UI

--- website/management/commands/dbinitialize.py
+++ website/management/commands/dbinitialize.py
@@ -17,3 +17,15 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS('%s property added.' % name))
         except Exception as ex:
             self.stdout.write(self.style.ERROR('Could not add %s property.' % name))
+        try:
+            name = 'arbatflat'
+            location = '55.743355, 37.579924'
+            contact = '1234567890'
+            email_id = 'random@random.com'
+            prop = Property.objects.create(name=name, location=location,
+                                           contact=contact, email_id=email_id)
+            prop.save()
+            self.stdout.write(self.style.SUCCESS('%s property added.' % name))
+        except Exception as ex:
+            self.stdout.write(self.style.ERROR('Could not add %s property.' % name))
+
