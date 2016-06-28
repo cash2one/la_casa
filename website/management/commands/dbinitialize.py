76a2bad159a26c3a4afb2a223708e217feef7749
Adi Krishnan <aditya.k2190@gmail.com>
2016-06-28 17:36:14 +0000

    updated utf-8 encoding, changed price to euros

--- website/management/commands/dbinitialize.py
+++ website/management/commands/dbinitialize.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
 from website.models import Property, Photos, SitetextEn, SitetextRu
 
@@ -17,22 +18,22 @@ class Command(BaseCommand):
             title = 'Cottage for sale near Moscow'
             about = 'Cottage for sale near Naro-Fominsk. 15 hectares of land in a protected small village. The house is 290 square meters and is made of bricks. There is an additional 100 square meter basement, and a large attic which can be turned into living space.'
             address = 'Russian Federation, Moscow Region, Naro-Fominsk'
-            price_dollars = '600,000'
+            price_euros = '600,000'
             price_roubles = '35,000,000'
             sitetext_en = SitetextEn.objects.create(name=name, title=title,
                                                     about=about, address=address,
-                                                    price_dollars=price_dollars,
+                                                    price_euros=price_euros,
                                                     price_roubles=price_roubles)
             sitetext_en.save()
             name = 'narofominsk'
             title = 'Продается элитный коттедж под Наро-фоминском (киевское шоссе)'
             about = 'Продается коттедж по киевскому направлению в районе Наро-фоминска. Участок 15 соток в охраняемом небольшом коттеджном поселке. Дом из кирпича 290 квадратных метров плюс 100 квадратных метров цокольного этажа, возможно обустройство мансарды.'
             address = 'Московская область, Наро-Фоминский район, садовое товарищество Осень, улица Профессора Арефьева 16'
-            price_dollars = '600,000'
+            price_euros = '600,000'
             price_roubles = '35,000,000'
             sitetext_ru = SitetextRu.objects.create(name=name, title=title,
                                                     about=about, address=address,
-                                                    price_dollars=price_dollars,
+                                                    price_euros=price_euros,
                                                     price_roubles=price_roubles)
             sitetext_ru.save()
             self.stdout.write(self.style.SUCCESS('%s property added.' % name))
