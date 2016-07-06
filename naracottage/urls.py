5703127f73758430baaf9b617ae5244731af8035
Adi Krishnan <aditya.k2190@gmail.com>
2016-07-06 15:11:03 +0000

    added russian and english support to backend

--- naracottage/urls.py
+++ naracottage/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from naracottage import views
 
 urlpatterns = [
-    url(r'^$', views.home),
+    url(r'^$', views.home_ru),
+    url(r'^ru/$', views.home_ru),
+    url(r'^en/$', views.home_en),
 ]
--- naracottage/views.py
+++ naracottage/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from website.models import Property, SitetextEn, Photos
+from website.models import Property, SitetextRu, SitetextEn, Photos
 
 
-def home(request):
+def home_ru(request):
     prop = Property.objects.get(name='narofominsk')
-    template = loader.get_template('naracottage_home.html')
+    template = loader.get_template('naracottage_ru.html')
+    location = prop.location
+    latitude, longitude = location.split(',')
+    photos = Photos.objects.filter(name='narofominsk')
+    photo_map = {}
+    for photo in photos:
+        photo_map[photo.alt_name] = photo.photo
+    sitetext_ru = SitetextRu.objects.get(name='narofominsk')
+    context = {
+                'logo_url': 'images/logo_naracottage.png',
+                'latitude': latitude,
+                'longitude': longitude,
+                'phone': prop.contact,
+                'title': sitetext_ru.title,
+                'about': sitetext_ru.about,
+                'address': sitetext_ru.address,
+                'price_euros': sitetext_ru.price_euros,
+                'price_roubles': sitetext_ru.price_roubles,
+                'photo_map': photo_map
+              }
+    return HttpResponse(template.render(context, request))
+
+
+def home_en(request):
+    prop = Property.objects.get(name='narofominsk')
+    template = loader.get_template('naracottage_en.html')
     location = prop.location
     latitude, longitude = location.split(',')
     photos = Photos.objects.filter(name='narofominsk')
@@ -27,3 +52,4 @@ def home(request):
                 'photo_map': photo_map
               }
     return HttpResponse(template.render(context, request))
+
